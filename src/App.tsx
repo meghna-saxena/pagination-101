b06a716180d722c9d01f445fc8b79474704f1fed
Meghna Srivastava <meghna.srivastava@auto1.com>
2019-07-08 16:58:29 +0200

    useEffect for fetching posts on component mount

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,21 @@ const App: React.FC = () => {
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(10);
-  
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      const result = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(result.data);
+
+      setLoading(false);
+    }
+
+    fetchPosts();
+  }, []);
+
+  console.log('post', posts);
   return (
     <div className="container">
       <h1>Pagination 101</h1>
